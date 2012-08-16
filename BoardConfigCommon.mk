0dc6542a4be4d97fa2c81390bcdae49196c15465
David Ferguson <ferguson.david@gmail.com>
2012-08-16 17:44:18 -0400

    msm8660: enable mounting of external sdcard in recovery
    Change-Id: I5ef1bc80cb20bdf1ac04494e95d4a5c313e3810f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -59,8 +59,10 @@ WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcm4330_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcm4330_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/wifi/bcm4330_p2p.bin"
 
+# Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/msm8660-common/recovery/graphics.c
+BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
